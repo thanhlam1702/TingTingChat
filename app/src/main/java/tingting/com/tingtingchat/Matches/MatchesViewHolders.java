78e0418a8b23d9e21249444ed01eea101ff9f0ba
npcv1999 <56424406+npcv1999@users.noreply.github.com>
2021-01-12 02:47:23 +0700

    chat&match

---
+++ app/src/main/java/tingting/com/tingtingchat/Matches/MatchesViewHolders.java
@@ -0,0 +1,35 @@
+package tingting.com.tingtingchat.Matches;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import tingting.com.tingtingchat.Chat.ChatActivity;
+import tingting.com.tingtingchat.R;
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle b = new Bundle();
+        b.putString("matchId", mMatchId.getText().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+    }
+}
